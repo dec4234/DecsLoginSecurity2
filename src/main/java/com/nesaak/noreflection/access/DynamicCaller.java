494e8cc3d864bdfead0c40636ce3cfd693d2a67a
dec4234 <decarround04@gmail.com>
2020-10-09 10:45:42 -0400

    Removing old stuff

--- src/main/java/com/nesaak/noreflection/access/DynamicCaller.java
+++
@@ -1,6 +0,0 @@
-package com.nesaak.noreflection.access;
-
-public interface DynamicCaller {
-
-    Object call(Object... parameters);
-}
--- src/main/java/com/nesaak/noreflection/access/FieldAccess.java
+++
@@ -1,9 +0,0 @@
-package com.nesaak.noreflection.access;
-
-public interface FieldAccess {
-
-    Object get(Object obj);
-
-    void set(Object obj, Object value);
-
-}
